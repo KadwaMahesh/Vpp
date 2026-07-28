e1eccc645fa961a7409024f009462fb281c64460
mahesh <choudharymahesh498@gmail.com>
2026-07-28 15:55:26 +0530

    Update project

--- lib/screens/personalInfo1.dart
+++ lib/screens/personalInfo1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:vpp/widgets/app_color.dart';
 import 'package:vpp/widgets/app_text_style.dart';
 
 class PersonalInfo extends StatelessWidget {
@@ -7,12 +9,44 @@ class PersonalInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: AppColor.pagebackground,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(112),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: AppColor.pagebackground,
+          elevation: 0,
+          flexibleSpace: SafeArea(
+            child: Padding(
+              padding: EdgeInsetsGeometry.only(left: 2, top: 18, right: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.arrow_back),
+                  ),
+                  SizedBox(height: 14),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Personal details',
+                      style: AppTextStyle.heading2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [Text('Personal details', style: AppTextStyle.heading2)],
-        ),
+        child: Column(children: []),
       ),
     );
   }
