0e3fa369db84b03032f0feb53b696e3fa19c252e
mahesh <choudharymahesh498@gmail.com>
2026-07-29 17:29:08 +0530

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vpp/screens/app_theme.dart';
 import 'package:vpp/screens/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://gsbqtfrhwsbbhnndsqzl.supabase.co',
+    publishableKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzYnF0ZnJod3NiYmhubmRzcXpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0OTM3ODMsImV4cCI6MjA5ODA2OTc4M30.nKE_50cwdrMOZtCP9KCivQu45eQFWqZSO-roBbFYkfE',
+  );
   runApp(const MyApp());
 }
 
